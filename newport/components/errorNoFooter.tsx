42eab400f6a34e790061bbcdf67b70d8186df057
CodeSandbox <support@codesandbox.io>
2023-07-06 06:49:35 +0000

    Initial commit

---
+++ newport/components/errorNoFooter.tsx
@@ -0,0 +1,12 @@
+const ErrorNoFooter = () => {
+  return (
+    <div >
+      {/* <h2 className='text-2xl text-red-600 font-semibold mb-8'>
+        Warning: there is no footer.
+      </h2>
+      <p className='mb-6'>React Bricks cannot find an entity for the footer.</p> */}
+    </div>
+  )
+}
+
+export default ErrorNoFooter
---
+++ newport/components/errorNoHeader.tsx
@@ -0,0 +1,12 @@
+const ErrorNoHeader = () => {
+  return (
+    <div >
+      {/* <h2 className='text-2xl text-red-600 font-semibold mb-8'> */}
+        {/* Warning: there is no header. */}
+      {/* </h2> */}
+      {/* <p className='mb-6'>React Bricks cannot find an entity for the header.</p> */}
+    </div>
+  )
+}
+
+export default ErrorNoHeader
